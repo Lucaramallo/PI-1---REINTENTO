dc5bb01187d97900e823e1b9503a66aa07407e56
Lucaramallo <95667827+Lucaramallo@users.noreply.github.com>
2023-05-29 09:52:09 -0300

    pandaeis

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ from pydantic import BaseModel
 
  # Cargar el archivo pickle en un DataFrame
 # df_merged = pd.read_pickle('../../Datasets/Datasets_cleaned_ETL/Combinado_merged_movies_ratings/df_merged.pkl')
-df_merged = pd.read_pickle('./df_merged.pkl')
+df_merged = pd.read_pickle('./Datasets/Datasets_cleaned_ETL/Combinado_merged_movies_ratings/df_merged.pkl')
 
 # with open('../PI-1---REINTENTO/df_merged.pkl') as file:
 #     df_merged = pickle.load(file)
